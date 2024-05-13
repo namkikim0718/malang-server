c68fe706396c5d2b81dd0808b7bb36cfbc4e9e8e
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-05-13 16:58:29 +0900

    fix: 필드 수정

--- src/main/java/com/example/malang/domain/Post.java
+++ src/main/java/com/example/malang/domain/Post.java
@@ -28,10 +28,6 @@ public class Post extends BaseEntity {
 
     private String content;
 
-    private int maleMembers;
-  
-    private int femaleMembers;
-
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
@@ -50,15 +46,13 @@ public class Post extends BaseEntity {
     private String storeFileName;
 
     @Builder
-    public Post(String title, String content, Member member, Place place, String uploadFileName, String storeFileName, int maleMembers, int femaleMembers) {
+    public Post(String title, String content, Member member, Place place, String uploadFileName, String storeFileName) {
         this.title = title;
         this.content = content;
         this.member = member;
         this.place = place;
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
-        this.maleMembers = maleMembers;
-        this.femaleMembers = femaleMembers;
     }
 
     public static Post of(PostRequestDto.PostRequest postRequest, Place place, Member member, String storeFileName, String uploadFileName) {
@@ -69,8 +63,6 @@ public class Post extends BaseEntity {
                 .place(place)
                 .uploadFileName(uploadFileName)
                 .storeFileName(storeFileName)
-                .maleMembers(postRequest.getMaleMembers())
-                .femaleMembers(postRequest.getFemaleMembers())
                 .build();
     }
 
--- src/main/java/com/example/malang/dto/PostRequestDto.java
+++ src/main/java/com/example/malang/dto/PostRequestDto.java
@@ -19,10 +19,6 @@ public class PostRequestDto {
 
         private String y;
 
-        private int maleMembers;
-
-        private int femaleMembers;
-
         private String content;
     }
 }
