ed2bfa8e4bbf876b21e11d32409c44ce8fa952b7
jd-hyun <wjdehdgus3540@naver.com>
2024-05-19 16:00:17 +0900

    update

--- src/main/java/com/example/malang/controller/ChatRoomController.java
+++ src/main/java/com/example/malang/controller/ChatRoomController.java
@@ -1,29 +1,30 @@
 package com.example.malang.controller;
 
+import com.example.malang.domain.ChatRoom;
+import com.example.malang.dto.ChatRoomRequest;
 import com.example.malang.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/v1")
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
 
-    @PostMapping("/api/v1/requests/{requestId}/rooms")
-    public Long createChatRoom(@PathVariable Long requestId) {
-        log.info("requestId : {}", requestId);
-        Long createChatRoom = chatRoomService.createChatRoom(requestId);
+    @PostMapping("/members/{memberId}/rooms")
+    public Long createChatRoom(@PathVariable("memberId") Long memberId, @RequestBody ChatRoomRequest chatRoomRequest) {
+        log.info("member1Id : {}", memberId);
+        log.info("member2Id: {}", chatRoomRequest.getId());
+        Long createChatRoom = chatRoomService.createChatRoom(memberId, chatRoomRequest.getId());
         log.info("createChatRoom : {}", createChatRoom);
         return createChatRoom;
     }
 
-    @DeleteMapping("/api/v1/rooms/{roomId}")
+    @DeleteMapping("/rooms/{roomId}")
     public void deleteChatRoom(@PathVariable Long roomId) {
         log.info("[roomId] = {}", roomId);
         chatRoomService.deleteChatRoom(roomId);
